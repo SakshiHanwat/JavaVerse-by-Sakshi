64f1e7adbdbd73c25cebe6758a8658ebfe2769ca
Sakshi Hanwat <hanwatsakshi900@gmail.com>
2025-10-25 09:41:35 +0000

    operator

---
+++ Java_Programs/unary.java
@@ -0,0 +1,23 @@
+public class unary{
+    public static void main(String[] args) {
+        int a = 10;
+        int b = ++a; //pre increment
+        System.out.println(a);
+        System.out.println(b);
+
+        int A = 10;
+        int B = a++; //post increment
+        System.out.println(A);
+        System.out.println(B);
+
+         int num1 = 10;
+        int num2 = ++a; //pre increment
+        System.out.println(num1);
+        System.out.println(num2);
+
+        int num3= 10;
+        int num4 = a++; //post increment
+        System.out.println(num3);
+        System.out.println(num4);
+    }
+}
