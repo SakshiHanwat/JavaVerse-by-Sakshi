255376b9c61f82e4b9bac20928719f4032a582b1
Sakshi Hanwat <hanwatsakshi900@gmail.com>
2025-10-25 07:48:13 +0000

    sum

---
+++ Java_Programs/Sum1.java
@@ -0,0 +1,8 @@
+public class Sum1 {
+    public static void main(String[]args){
+        int a = 10;
+        int b = 5;
+        int sum = a + b;
+        System.out.println("Sum = "+sum);
+    }
+}
