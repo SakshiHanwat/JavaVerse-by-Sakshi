e9d567daf6618abd2c86db2a30744aaa8250aa21
Sakshi Hanwat <hanwatsakshi900@gmail.com>
2025-10-25 03:16:04 +0000

    hello java

---
+++ Java_Programs/Output.java
@@ -0,0 +1,10 @@
+public class Output{
+    public static void main(String []args){
+        //Output in java --three way 
+        System.out.print("Hello World");
+        System.out.println("Hello World");
+        System.out.print("Hello World\n");
+        System.out.print("Hello World\n Sakshi");
+
+    }
+}
