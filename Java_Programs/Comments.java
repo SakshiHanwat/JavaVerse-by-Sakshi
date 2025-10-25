c170b33519edc9abdcfc5d4e8632ed43e14ba3e4
Sakshi Hanwat <hanwatsakshi900@gmail.com>
2025-10-25 07:54:40 +0000

    comments

---
+++ Java_Programs/Comments.java
@@ -0,0 +1,11 @@
+public class Comments {
+    public static void main(String[]args){
+        /*Comments in java --two way 
+         * 
+         * first single line comment 
+         * second multiple line comment 
+         */
+        //single line comment 
+        /*multiple line comment  */
+    }
+}
