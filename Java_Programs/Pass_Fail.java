b9ddef84109bf35c78fbc25e49181ecaadde345b
Sakshi Hanwat <hanwatsakshi900@gmail.com>
2025-10-26 08:07:01 +0000

    ternary

---
+++ Java_Programs/Pass_Fail.java
@@ -0,0 +1,11 @@
+import java.util.*;
+public class Pass_Fail{
+    public static void main(String[]args){
+        Scanner sc = new Scanner(System.in);
+
+        int marks = sc.nextInt();
+
+        String result = (marks >= 33) ? "PASS" : "FAIL";
+        System.out.println(result);
+    }
+}
