38b4df1f2cc489b378d282e806bbcf70ca357874
Sakshi Hanwat <hanwatsakshi900@gmail.com>
2025-10-26 09:54:56 +0000

    break

---
+++ Java_Programs/Break1.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class Break1 {
+    public static void main(String[] args) {
+        //Keep entering numbers till user enters a multiple of 10
+        Scanner sc = new Scanner(System.in);
+         
+        do {
+            System.out.print("enter your number: ");
+            int n = sc.nextInt();
+            if(n%10 ==0){
+                break;
+            }
+            System.out.println(n);
+        }while(true);
+    }
+}
