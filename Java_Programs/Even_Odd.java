c9ac476dcc1f1380d84423f6a46c5fa842fbc735
Sakshi Hanwat <hanwatsakshi900@gmail.com>
2025-10-26 07:18:33 +0000

    conditional

--- Java_Programs/Even_Odd.java
+++ Java_Programs/Even_Odd.java
@@ -4,13 +4,12 @@ public class Even_Odd{
         Scanner sc = new Scanner(System.in);
 
         int num1 = sc.nextInt();
-        int num2 = sc.nextInt();
 
-        if(num1 >num2){
-            System.out.println("Num1: "+ num1 +" is greater than "+"Num2: "+num2);
+        if(num1 % 2 ==0){
+            System.out.println("EVEN");
         }
         else{
-                        System.out.println("Num2: "+ num2 +" is greater than "+"Num1: "+num1);
+            System.out.println("ODD");
 
         }
     }
