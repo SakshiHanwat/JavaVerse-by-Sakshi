e5835b7266e661c65773f02cd55c36171ae108aa
Sakshi Hanwat <hanwatsakshi900@gmail.com>
2025-10-28 04:49:27 +0000

    practice problems

---
+++ Java_Programs/Area_Square.java
@@ -0,0 +1,10 @@
+import java.util.*;
+
+public class Area_Square {
+    public static void main(String[]args){
+        Scanner sc = new Scanner(System.in);
+        int side = sc.nextInt();
+        int Area = side*side;
+        System.out.println(Area);
+    }
+}
