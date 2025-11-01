db1a1095f6d67d1409a7ae743bec9c1e7492c5ed
Sakshi Hanwat <hanwatsakshi900@gmail.com>
2025-11-01 05:39:12 +0000

    array

---
+++ Java_Programs/ArraysCC.java
@@ -0,0 +1,16 @@
+import java.util.*;
+
+public class ArraysCC 
+{
+    public static void main(String[]args){
+
+        //Creation of an array
+        int marks[] = new int[50];
+        int marks1 []={3, 4 ,5 };
+
+        String fruits[] = {"Mango","Graphes","Banana"};
+
+        float nums[] = {0.2f, 0.3f, 9.56f};
+        
+}
+}
