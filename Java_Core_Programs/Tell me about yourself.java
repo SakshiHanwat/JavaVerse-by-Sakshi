8cc05dd6027eebdcfdc7028b31399c18d451f98f
Sakshi Hanwat <hanwatsakshi900@gmail.com>
2025-12-17 23:47:19 +0530

    Add files via upload

---
+++ Java_Core_Programs/Tell me about yourself.java
@@ -0,0 +1,21 @@
+class Tell me about yourself 
+{
+	public static void main(String[] args)
+{
+	System.out.println("Hii, I am Sakshi Hanwat.");
+	System.out.println("I am completed both my 10th and 12th from Saraswati Gyan Peeth Higher Sec. School.");
+	System.out.println("Currently I am persuing B.tech (cse) from Lakshmi Narain College of Technology and Science.");
+	System.out.println("I was born and brought up in Dewas.");
+	System.out.println("I am single daughter of my parents.");
+	System.out.println("My father name is Yashwant Hanwat.");
+	System.out.println("I am also good in other activities like sketching, drawing etc.");
+	Systme.out.println("My hobby is book reading , music listening, travelling in free time.");
+	System.out.println("I am also achieved  many rewards in art curriculum.");
+	System.out.println("I am also good in study and achieved many rewards and certificates.");
+	System.out.println("In future , I want to become  a software devloper.");
+	Systme.out.println("My strength is I am hardworking ,I have been very much passionate about coding.");
+	System.out.println("My weakness is fear of public speaking.");
+	System.out.println("I am fully dedicated for my future goal.");
+    }
+}
+	
