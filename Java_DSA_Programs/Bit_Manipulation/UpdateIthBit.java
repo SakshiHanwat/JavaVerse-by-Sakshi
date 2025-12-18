15eb25c31dff8a1c980343d5d2492dac1362cf5b
Sakshi Hanwat <hanwatsakshi900@gmail.com>
2025-12-18 18:36:10 +0000

    Bit Manipulation operations

--- Java_DSA_Programs/Bit_Manipulation/UpdateIthBit.java
+++ Java_DSA_Programs/Bit_Manipulation/UpdateIthBit.java
@@ -1,21 +1,27 @@
-public class UpdateIthBit{
-    public static int SetIthBit(int n , int i){
-        int bitmask = 1<<i;
-        return n|bitmask;
+public class UpdateIthBit {
+
+    // Set ith bit (make it 1)
+    public static int SetIthBit(int n, int i) {
+        int bitmask = 1 << i;
+        return n | bitmask;
     }
-     public static int ClearIthBitIthBit(int n , int i){
-        int bitmask = ~(1<<i);
-        return n&bitmask;
+
+    // Clear ith bit (make it 0)
+    public static int ClearIthBit(int n, int i) {
+        int bitmask = ~(1 << i);
+        return n & bitmask;
     }
-   
-    public static int UpdateIthBit(int n , int i,int newBit){
-        if(newbit == 0){
-            return clearIthBit(n,i);
+
+    // Update ith bit according to newBit (0 or 1)
+    public static int UpdateIthBit(int n, int i, int newBit) {
+        if (newBit == 0) {
+            return ClearIthBit(n, i);
+        } else {
+            return SetIthBit(n, i);
         }
-        int bitmask = 1<<i;
-        return n|bitmask;
     }
+
     public static void main(String[] args) {
-        System.out.println(UpdateIthBit(10,2,1));
+        System.out.println(UpdateIthBit(10, 2, 1));
     }
 }
