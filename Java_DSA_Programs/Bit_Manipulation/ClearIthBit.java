537b25287c94e5e8ff25eefab30a389de6010453
Sakshi Hanwat <hanwatsakshi900@gmail.com>
2025-12-18 18:27:30 +0000

    Bit manipulation operations

---
+++ Java_DSA_Programs/Bit_Manipulation/ClearIthBit.java
@@ -0,0 +1,9 @@
+public class ClearIthBit{
+    public static int SetIthBit(int n , int i){
+        int bitmask = ~(1<<i);
+        return n&bitmask;
+    }
+    public static void main(String[] args) {
+        System.out.println(SetIthBit(10,2));
+    }
+}
