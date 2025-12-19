7b0d1643e127768c2aef446bd1631b5d5157e31f
Sakshi Hanwat <172370156+SakshiHanwat@users.noreply.github.com>
2025-12-19 18:22:26 +0000

    packages

---
+++ Java_Core_Programs/OOPS/PackagesDemo.java
@@ -0,0 +1,13 @@
+// Simulated package example (in real projects use 'package mypackage;')
+class PackageDemo {
+    void show() {
+        System.out.println("Package concept demo");
+    }
+}
+
+public class PackagesDemo {
+    public static void main(String[] args) {
+        PackageDemo obj = new PackageDemo();
+        obj.show();
+    }
+}
