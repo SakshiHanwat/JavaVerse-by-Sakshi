426ee762e21276ad21b9d4dc237e36e04a907903
Sakshi Hanwat <172370156+SakshiHanwat@users.noreply.github.com>
2025-12-19 17:42:41 +0000

    constructor

---
+++ Java_Core_Programs/OOPS/Default_Constructor.java
@@ -0,0 +1,15 @@
+import java.io.*;
+
+class Default_Constructor{
+
+    // Default Constructor
+    Default_Constructor(){
+        
+        System.out.println("Default constructor"); 
+        
+    }
+    public static void main(String[] args){
+        
+        Default_Constructor hello = new Default_Constructor();
+    }
+}
