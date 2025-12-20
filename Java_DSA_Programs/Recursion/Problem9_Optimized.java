d086eca4bdc8cc720654b029732222338a05ebb1
Sakshi Hanwat <172370156+SakshiHanwat@users.noreply.github.com>
2025-12-20 18:44:53 +0000

    recursion

---
+++ Java_DSA_Programs/Recursion/Problem9_Optimized.java
@@ -0,0 +1,20 @@
+// optimized power calculation
+public class Problem9_Optimized{
+    public static int OptimizedPower(int a , int n){
+        if(n==0){
+            return 1;
+        }
+        int halfPower = OptimizedPower(a,n/2);
+        int halfPowerSq = halfPower*halfPower;
+        //n is odd 
+        if(n%2 !=0){
+            halfPowerSq = a*halfPowerSq;
+        }
+        return halfPowerSq;
+    }
+    public static void main(String[] args) {
+        int a = 2;
+        int n = 5;
+        System.out.println(OptimizedPower(a,n));
+    }
+}
