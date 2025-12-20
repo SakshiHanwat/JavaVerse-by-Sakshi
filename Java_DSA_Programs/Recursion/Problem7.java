e9d85c4f0f90d047ae439871efecbc26514d3dd7
Sakshi Hanwat <172370156+SakshiHanwat@users.noreply.github.com>
2025-12-20 18:23:18 +0000

    recursion

---
+++ Java_DSA_Programs/Recursion/Problem7.java
@@ -0,0 +1,17 @@
+//WAP to find the first occurence of an element in an array
+
+public class Problem7{
+    public static int FirstOccurence(int arr[], int key, int i){
+        if(i == arr.length){
+            return -1;
+        }
+        if(arr[i]==key){
+            return 1;
+        }
+        return FirstOccurence(arr,key,i+1);
+    }
+    public static void main(String[] args) {
+        int arr[] = {8,3,6,9,5,10,2,5,3};
+        System.out.println(FirstOccurence(arr,5,0));
+    }
+}
