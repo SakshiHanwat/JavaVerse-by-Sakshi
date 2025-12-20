8be81e67e656a3322aff80dcd6b8b10157d16ae7
Sakshi Hanwat <172370156+SakshiHanwat@users.noreply.github.com>
2025-12-20 18:38:23 +0000

    recursion

---
+++ Java_DSA_Programs/Recursion/Problem9.java
@@ -0,0 +1,20 @@
+// Print x to the power n Print x^n
+
+public class Problem9{
+    public static int power(int x, int n){
+        if(n==0){
+            return 1;
+        }
+        /*
+        int xnm1 = power(x ,n-1);
+        int xn = x*xnm1;
+        return xn;
+         */
+
+        //Shorter version
+        return x*power(x, n-1);
+    }
+    public static void main(String[]args){
+        System.out.println(power(2,10));
+    }
+}
