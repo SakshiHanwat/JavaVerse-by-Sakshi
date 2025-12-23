0f79fa0a33d59d0082d9fdbe5962f722dfa93834
Sakshi Hanwat <172370156+SakshiHanwat@users.noreply.github.com>
2025-12-23 17:43:02 +0000

    arraylist

---
+++ Java_DSA_Programs/ArrayList/ReverseOf_ArrayList.java
@@ -0,0 +1,22 @@
+import java.util.ArrayList;
+
+public class ReverseOf_ArrayList{
+    public static void main(String[] args) {
+        //Java Collection Framework
+        ArrayList<Integer> list = new ArrayList<>();
+        
+        //Add element
+        list.add(1);
+        list.add(2);
+        list.add(3);
+        list.add(4);
+        list.add(5);
+        list.add(6);
+         
+        //reverse print
+        for(int i  = list.size()-1; i>=0; i--){
+        System.out.print(list.get(i)+ " ");
+        }
+        System.out.println();
+    }
+}
