3e2f7fc7c6594b4b5b2ec12e6a298f8b6b1d2ab0
Sakshi Hanwat <172370156+SakshiHanwat@users.noreply.github.com>
2025-12-23 15:47:24 +0000

    backtracking

---
+++ Java_DSA_Programs/BackTracking/BackTrackingOnArrays.java
@@ -0,0 +1,25 @@
+public class BackTrackingOnArrays{
+    public static void ChangeOnArray(int arr[], int i , int val){
+        //base case 
+        if(i == arr.length){
+            printArr(arr);
+            return;
+        }
+
+        //recursion
+        arr[i] = val;
+        ChangeOnArray(arr, i+1, val+1); //fnx call step
+        arr[i] = arr[i]-2; //backtracking step
+    }
+    public static void printArr(int arr[]){
+        for(int i = 0; i < arr.length; i++){
+            System.out.print(arr[i]+" ");
+        }
+        System.out.println();
+    }
+    public static void main(String[] args) {
+        int arr[]= new int[5];
+        ChangeOnArray(arr,0,1);
+        printArr(arr);
+    }
+}
