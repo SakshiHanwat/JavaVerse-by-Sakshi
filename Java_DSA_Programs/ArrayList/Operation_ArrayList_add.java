cadd377149fa90b1b4183f9d0048b887f56a9e22
Sakshi Hanwat <172370156+SakshiHanwat@users.noreply.github.com>
2025-12-23 17:21:10 +0000

    arraylist

---
+++ Java_DSA_Programs/ArrayList/Operation_ArrayList_add.java
@@ -0,0 +1,18 @@
+import java.util.ArrayList;
+
+public class Operation_ArrayList_add{
+    public static void main(String[] args) {
+        //Java Collection Framework
+        ArrayList<Integer> list = new ArrayList<>();
+        
+        //Add element
+        list.add(1);
+        list.add(2);
+        list.add(3);
+        list.add(4);
+        list.add(5);
+        list.add(6);
+         
+        System.out.println(list);
+    }
+}
