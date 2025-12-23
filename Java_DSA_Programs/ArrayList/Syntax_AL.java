ba4c4e451b40be6f031ae88090fad43dc6692df0
Sakshi Hanwat <172370156+SakshiHanwat@users.noreply.github.com>
2025-12-23 17:16:27 +0000

    arraylist

---
+++ Java_DSA_Programs/ArrayList/Syntax_AL.java
@@ -0,0 +1,12 @@
+import java.util.ArrayList;
+
+public class Syntax_AL{
+    public static void main(String[] args) {
+        //Java Collection Framework
+        ArrayList<Integer> list = new ArrayList<>();
+        ArrayList<String> list1 = new ArrayList<>();
+        ArrayList<Boolean> list2 = new ArrayList<>();
+        ArrayList<Float> list3 = new ArrayList<>();
+
+    }
+}
