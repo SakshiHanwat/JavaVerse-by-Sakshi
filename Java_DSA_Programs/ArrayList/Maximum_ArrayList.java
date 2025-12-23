dbced025d50f04887c9eacbb05ff10ef4ed69f5d
Sakshi Hanwat <172370156+SakshiHanwat@users.noreply.github.com>
2025-12-23 17:47:48 +0000

    arraylist

---
+++ Java_DSA_Programs/ArrayList/Maximum_ArrayList.java
@@ -0,0 +1,26 @@
+import java.util.ArrayList;
+
+public class Maximum_ArrayList{
+    public static void main(String[] args) {
+        //Java Collection Framework
+        ArrayList<Integer> list = new ArrayList<>();
+        
+        //Add element
+        list.add(2);
+        list.add(5);
+        list.add(9);
+        list.add(3);
+        list.add(6);
+        list.add(7);
+         
+        //maximum
+        int max = Integer.MIN_VALUE;
+        for(int i =0; i <list.size(); i++){
+            if(max<list.get(i)){
+                max = list.get(i);
+            }
+            //max =Math.max(max, list.get(i));
+        }
+        System.out.println("Max element = " +max);
+    }
+}
