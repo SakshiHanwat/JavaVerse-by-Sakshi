3efafe8a7af8504429bd9eddebbf5c263a76e632
Sakshi Hanwat <172370156+SakshiHanwat@users.noreply.github.com>
2025-12-24 17:44:43 +0000

    linkedlist

---
+++ Java_DSA_Programs/LinkedList/IterativeSearch_LL.java
@@ -0,0 +1,48 @@
+public class IterativeSearch_LL {
+
+    public static class Node {
+        int data;
+        Node next;
+
+        public Node(int data) {
+            this.data = data;
+            this.next = null;
+        }
+    }
+
+    public static Node head;
+    public static Node tail;
+
+    public void addLast(int data) {
+        Node newNode = new Node(data);
+        if (head == null) {
+            head = tail = newNode;
+            return;
+        }
+        tail.next = newNode;
+        tail = newNode;
+    }
+
+    public int itrSearch(int key) {
+        Node temp = head;
+        int i = 0;
+
+        while (temp != null) {
+            if (temp.data == key) {
+                return i;
+            }
+            temp = temp.next;
+            i++;
+        }
+        return -1;
+    }
+
+    public static void main(String[] args) {
+        IterativeSearch_LL ll = new IterativeSearch_LL();
+        ll.addLast(10);
+        ll.addLast(20);
+        ll.addLast(30);
+
+        System.out.println(ll.itrSearch(20));
+    }
+}
