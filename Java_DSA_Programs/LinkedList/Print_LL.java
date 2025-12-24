71818f24bf2e5b23c99fc33e48cd0e7f105350fe
Sakshi Hanwat <172370156+SakshiHanwat@users.noreply.github.com>
2025-12-24 17:42:29 +0000

    linkedlist

--- Java_DSA_Programs/LinkedList/Print_LL.java
+++ Java_DSA_Programs/LinkedList/Print_LL.java
@@ -1,65 +1,63 @@
-public class Print_LL{
-    public static class Node{
+public class RecursiveSearchLL {
+
+    public static class Node {
         int data;
         Node next;
 
-        public Node(int data){
-            this.data =data;
+        public Node(int data) {
+            this.data = data;
             this.next = null;
         }
     }
+
     public static Node head;
     public static Node tail;
 
-    public void addFirst(int data){
-        //Step1 = create new node
+    public void addLast(int data) {
         Node newNode = new Node(data);
-
-        //case 3
-        if(head == null){
-            head = tail = newNode;
-            return;
-        }
-        //Step2 = newNode next = head
-        newNode.next = head;  //link
-
-        //Step3 = head = newNode
-        head = newNode;
-
-    }
-
-    public void addLast(int data){
-        Node newNode = new Node(data);
-        if(head == null){
+        if (head == null) {
             head = tail = newNode;
             return;
         }
         tail.next = newNode;
         tail = newNode;
     }
-    public void print(){
-        if(head == null){
-            System.out.println("linkedList is empty");
-            return;
+
+    public int helper(Node head, int key) {
+        if (head == null) {
+            return -1;
         }
-        Node temp = head; 
-        while(temp != null){
-            System.out.print(temp.data+"--->");
+        if (head.data == key) {
+            return 0;
+        }
+        int idx = helper(head.next, key);
+        if (idx == -1) {
+            return -1;
+        }
+        return idx + 1;
+    }
+
+    public int recSearch(int key) {
+        return helper(head, key);
+    }
+
+    public void print() {
+        Node temp = head;
+        while (temp != null) {
+            System.out.print(temp.data + "--->");
             temp = temp.next;
         }
         System.out.println("null");
     }
 
-    public static void main(String[]args){
-        Print_LL ll = new Print_LL();
-        ll.print();
-        ll.addFirst(1);
-        ll.print();
-        ll.addFirst(2);
-        ll.print();
+    public static void main(String[] args) {
+        RecursiveSearchLL ll = new RecursiveSearchLL();
+        ll.addLast(1);
+        ll.addLast(2);
         ll.addLast(3);
-        ll.print();
         ll.addLast(4);
+
         ll.print();
+        System.out.println(ll.recSearch(3));
     }
 }
---
+++ Java_DSA_Programs/LinkedList/RecursiveSearchLL.java
@@ -0,0 +1,42 @@
+public class RecursiveSearchLL {
+
+    static class Node {
+        int data;
+        Node next;
+
+        Node(int data) {
+            this.data = data;
+            this.next = null;
+        }
+    }
+
+    static Node head;
+
+    public static int helper(Node head, int key) {
+        if (head == null) {
+            return -1;
+        }
+        if (head.data == key) {
+            return 0;
+        }
+
+        int idx = helper(head.next, key);
+        if (idx == -1) {
+            return -1;
+        }
+        return idx + 1;
+    }
+
+    public static int recSearch(int key) {
+        return helper(head, key);
+    }
+
+    public static void main(String[] args) {
+        head = new Node(1);
+        head.next = new Node(2);
+        head.next.next = new Node(3);
+        head.next.next.next = new Node(4);
+
+        System.out.println(recSearch(3)); // output: 2
+    }
+}
