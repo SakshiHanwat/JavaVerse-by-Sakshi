d8a0890cff563d45456449b155b9ab792da765d4
Sakshi Hanwat <172370156+SakshiHanwat@users.noreply.github.com>
2025-12-24 14:30:28 +0000

    linkedlist

---
+++ Java_DSA_Programs/LinkedList/Creation_LL.java
@@ -0,0 +1,16 @@
+public class Creation_LL{
+    public static class Node{
+        int data;       //value
+        Node next;       //next node address
+
+        //Constructor
+        public Node(int data){
+            this.data = data;
+            this.next = null;
+        }
+
+    }
+    public static void main(String[]args){
+
+    }
+}
