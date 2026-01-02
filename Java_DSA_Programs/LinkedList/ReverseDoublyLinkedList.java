db8117569ac3338feca15891ac74cfd0effe160c
Sakshi Hanwat <172370156+SakshiHanwat@users.noreply.github.com>
2026-01-02 04:49:10 +0000

    linkedlist

---
+++ Java_DSA_Programs/LinkedList/ReverseDoublyLinkedList.java
@@ -0,0 +1,68 @@
+public class ReverseDoublyLinkedList {
+
+    static class Node {
+        int data;
+        Node next;
+        Node prev;
+
+        Node(int data) {
+            this.data = data;
+            this.next = null;
+            this.prev = null;
+        }
+    }
+
+    public static Node head;
+    public static Node tail;
+
+    public void addFirst(int data) {
+        Node newNode = new Node(data);
+
+        if (head == null) {
+            head = tail = newNode;
+            return;
+        }
+
+        newNode.next = head;
+        head.prev = newNode;
+        head = newNode;
+    }
+
+    public void print() {
+        Node temp = head;
+        while (temp != null) {
+            System.out.print(temp.data + "<->");
+            temp = temp.next;
+        }
+        System.out.println("null");
+    }
+
+    public void reverse() {
+        Node curr = head;
+        Node prev = null;
+        Node next;
+
+        while (curr != null) {
+            next = curr.next;
+            curr.next = prev;
+            curr.prev = next;
+
+            prev = curr;
+            curr = next;
+        }
+
+        head = prev;
+    }
+
+    public static void main(String[] args) {
+        ReverseDoublyLinkedList dll = new ReverseDoublyLinkedList();
+
+        dll.addFirst(3);
+        dll.addFirst(2);
+        dll.addFirst(1);
+
+        dll.print();
+        dll.reverse();
+        dll.print();
+    }
+}
