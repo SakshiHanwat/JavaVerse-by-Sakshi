ec7d65d59d774e40a8de5530ca9322dc30e4804c
Sakshi Hanwat <172370156+SakshiHanwat@users.noreply.github.com>
2026-01-03 08:54:49 +0000

    trie

---
+++ Java_DSA_Programs/Trie/TrieInsert.java
@@ -0,0 +1,37 @@
+public class TrieInsert {
+
+    static class Node {
+        Node[] children = new Node[26];
+        boolean eow = false;
+
+        Node() {
+            for (int i = 0; i < 26; i++) {
+                children[i] = null;
+            }
+        }
+    }
+
+    static Node root = new Node();
+
+    public static void insert(String word) {
+        Node curr = root;
+
+        for (int i = 0; i < word.length(); i++) {
+            int idx = word.charAt(i) - 'a';
+
+            if (curr.children[idx] == null) {
+                curr.children[idx] = new Node();
+            }
+            curr = curr.children[idx];
+        }
+        curr.eow = true;
+    }
+
+    public static void main(String[] args) {
+        insert("apple");
+        insert("app");
+        insert("mango");
+
+        System.out.println("Words inserted successfully");
+    }
+}
