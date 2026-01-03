e676a969c73b9e3422896d48cff04dae43d15f44
Sakshi Hanwat <172370156+SakshiHanwat@users.noreply.github.com>
2026-01-03 08:13:27 +0000

    hashmap

---
+++ Java_DSA_Programs/HashMap/TreeMapDemo.java
@@ -0,0 +1,14 @@
+import java.util.*;
+
+public class TreeMapDemo {
+    public static void main(String[] args) {
+
+        TreeMap<String, Integer> tm = new TreeMap<>();
+
+        tm.put("India", 100);
+        tm.put("China", 150);
+        tm.put("US", 50);
+
+        System.out.println(tm); // sorted order
+    }
+}
