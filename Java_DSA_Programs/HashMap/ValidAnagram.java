a85418be729a5a76b5ab3b6618683548d1cf77b1
Sakshi Hanwat <172370156+SakshiHanwat@users.noreply.github.com>
2026-01-03 08:16:06 +0000

    hashmap

---
+++ Java_DSA_Programs/HashMap/ValidAnagram.java
@@ -0,0 +1,31 @@
+import java.util.*;
+
+public class ValidAnagram {
+
+    public static boolean isAnagram(String s, String t) {
+
+        if (s.length() != t.length()) return false;
+
+        HashMap<Character, Integer> map = new HashMap<>();
+
+        for (char ch : s.toCharArray()) {
+            map.put(ch, map.getOrDefault(ch, 0) + 1);
+        }
+
+        for (char ch : t.toCharArray()) {
+            if (!map.containsKey(ch)) return false;
+
+            if (map.get(ch) == 1)
+                map.remove(ch);
+            else
+                map.put(ch, map.get(ch) - 1);
+        }
+
+        return map.isEmpty();
+    }
+
+    public static void main(String[] args) {
+        System.out.println(isAnagram("race", "care")); // true
+        System.out.println(isAnagram("tulip", "lipid")); // false
+    }
+}
