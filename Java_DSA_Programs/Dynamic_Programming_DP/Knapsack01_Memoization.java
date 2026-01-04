f65c5480973a3140f292e05af311000bd0a125a6
Sakshi Hanwat <172370156+SakshiHanwat@users.noreply.github.com>
2026-01-04 09:20:03 +0000

    dp

---
+++ Java_DSA_Programs/Dynamic_Programming_DP/Knapsack01_Memoization.java
@@ -0,0 +1,33 @@
+public class Knapsack01_Memoization {
+
+    public static int knapsack(int val[], int wt[], int W, int n, int dp[][]) {
+        if (W == 0 || n == 0) return 0;
+
+        if (dp[n][W] != -1) return dp[n][W];
+
+        if (wt[n - 1] <= W) {
+            dp[n][W] = Math.max(
+                    val[n - 1] + knapsack(val, wt, W - wt[n - 1], n - 1, dp),
+                    knapsack(val, wt, W, n - 1, dp)
+            );
+        } else {
+            dp[n][W] = knapsack(val, wt, W, n - 1, dp);
+        }
+        return dp[n][W];
+    }
+
+    public static void main(String[] args) {
+        int val[] = {15, 14, 10, 45, 30};
+        int wt[] = {2, 5, 1, 3, 4};
+        int W = 7;
+
+        int dp[][] = new int[val.length + 1][W + 1];
+        for (int i = 0; i < dp.length; i++) {
+            for (int j = 0; j < dp[0].length; j++) {
+                dp[i][j] = -1;
+            }
+        }
+
+        System.out.println(knapsack(val, wt, W, val.length, dp));
+    }
+}
