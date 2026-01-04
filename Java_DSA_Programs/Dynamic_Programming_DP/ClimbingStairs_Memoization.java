a8f5bf2528f0bf63dc926db15f5fca8e14ae138f
Sakshi Hanwat <172370156+SakshiHanwat@users.noreply.github.com>
2026-01-04 09:16:52 +0000

    dp

---
+++ Java_DSA_Programs/Dynamic_Programming_DP/ClimbingStairs_Memoization.java
@@ -0,0 +1,18 @@
+public class ClimbingStairs_Memoization {
+
+    public static int ways(int n, int dp[]) {
+        if (n == 0) return 1;
+        if (n < 0) return 0;
+
+        if (dp[n] != 0) return dp[n];
+
+        dp[n] = ways(n - 1, dp) + ways(n - 2, dp);
+        return dp[n];
+    }
+
+    public static void main(String[] args) {
+        int n = 5;
+        int dp[] = new int[n + 1];
+        System.out.println(ways(n, dp));
+    }
+}
