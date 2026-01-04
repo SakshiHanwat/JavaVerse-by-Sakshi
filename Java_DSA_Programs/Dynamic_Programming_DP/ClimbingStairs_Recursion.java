0bf2774f3e4bb62140d4b7029cf60fc2af01c27b
Sakshi Hanwat <172370156+SakshiHanwat@users.noreply.github.com>
2026-01-04 09:15:32 +0000

    dp

---
+++ Java_DSA_Programs/Dynamic_Programming_DP/ClimbingStairs_Recursion.java
@@ -0,0 +1,14 @@
+public class ClimbingStairs_Recursion {
+
+    public static int ways(int n) {
+        if (n == 0) return 1;
+        if (n < 0) return 0;
+
+        return ways(n - 1) + ways(n - 2);
+    }
+
+    public static void main(String[] args) {
+        int n = 5;
+        System.out.println(ways(n));
+    }
+}
