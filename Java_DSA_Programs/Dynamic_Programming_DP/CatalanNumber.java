8e1ca27e9ce1cd512623b86040610952039201b3
Sakshi Hanwat <172370156+SakshiHanwat@users.noreply.github.com>
2026-01-04 09:46:38 +0000

    dp

---
+++ Java_DSA_Programs/Dynamic_Programming_DP/CatalanNumber.java
@@ -0,0 +1,29 @@
+public class CatalanNumber {
+
+    public static int catalan(int n, int[] dp) {
+        if (n == 0 || n == 1) {
+            return 1;
+        }
+
+        if (dp[n] != -1) {
+            return dp[n];
+        }
+
+        int ans = 0;
+        for (int i = 0; i < n; i++) {
+            ans += catalan(i, dp) * catalan(n - i - 1, dp);
+        }
+        return dp[n] = ans;
+    }
+
+    public static void main(String[] args) {
+        int n = 4;
+        int[] dp = new int[n + 1];
+
+        for (int i = 0; i <= n; i++) {
+            dp[i] = -1;
+        }
+
+        System.out.println(catalan(n, dp));
+    }
+}
