8a29424007988f3a1f3ac6d16acaa32561b75745
Sakshi Hanwat <172370156+SakshiHanwat@users.noreply.github.com>
2026-01-04 09:18:59 +0000

    dp

---
+++ Java_DSA_Programs/Dynamic_Programming_DP/Knapsack01_Tabulation.java
@@ -0,0 +1,26 @@
+public class Knapsack01_Tabulation {
+
+    public static void main(String[] args) {
+        int val[] = {15, 14, 10, 45, 30};
+        int wt[] = {2, 5, 1, 3, 4};
+        int W = 7;
+
+        int n = val.length;
+        int dp[][] = new int[n + 1][W + 1];
+
+        for (int i = 1; i <= n; i++) {
+            for (int w = 1; w <= W; w++) {
+                if (wt[i - 1] <= w) {
+                    dp[i][w] = Math.max(
+                            val[i - 1] + dp[i - 1][w - wt[i - 1]],
+                            dp[i - 1][w]
+                    );
+                } else {
+                    dp[i][w] = dp[i - 1][w];
+                }
+            }
+        }
+
+        System.out.println(dp[n][W]);
+    }
+}
