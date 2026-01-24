06b664310a835c20cd976ef6c6458025fc82b4a9
Sakshi Hanwat <172370156+SakshiHanwat@users.noreply.github.com>
2026-01-24 22:17:23 +0530

    Create DataTypesExample.java

---
+++ Java_Core_Programs/01_DataTypes_Variables/DataTypesExample.java
@@ -0,0 +1,28 @@
+public class DataTypesExample {
+    public static void main(String[] args) {
+
+        // Integer type
+        int age = 20;
+
+        // Floating point type
+        float height = 5.4f;
+        double salary = 45000.75;
+
+        // Character type
+        char grade = 'A';
+
+        // Boolean type
+        boolean isStudent = true;
+
+        // String (non-primitive)
+        String name = "Sakshi";
+
+        // Printing values
+        System.out.println("Name: " + name);
+        System.out.println("Age: " + age);
+        System.out.println("Height: " + height);
+        System.out.println("Salary: " + salary);
+        System.out.println("Grade: " + grade);
+        System.out.println("Is Student: " + isStudent);
+    }
+}
