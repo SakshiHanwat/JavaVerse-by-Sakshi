e0edb49dc76649076ba0f30ccd2c3da42eed8279
Sakshi Hanwat <172370156+SakshiHanwat@users.noreply.github.com>
2026-01-25 09:22:33 +0000

    1st day of leetcode

---
+++ 100_Days_Leetcode/Palindrome_9LC_01.java
@@ -0,0 +1,34 @@
+public class Palindrome_9LC_01 {
+
+    public boolean isPalindrome(int x) {
+        int n = x;
+
+        // Negative numbers are not palindrome
+        if (n < 0) {
+            return false;
+        }
+
+        int revNum = 0;
+
+        while (n > 0) {
+            int d = n % 10;
+            revNum = revNum * 10 + d;
+            n = n / 10;
+        }
+
+        return revNum == x;
+    }
+
+    public static void main(String[] args) {
+        Palindrome_9LC_01 sol = new Palindrome_9LC_01();
+
+        int number = 121;   // you can change this value
+        boolean result = sol.isPalindrome(number);
+
+        if (result) {
+            System.out.println(number + " is a Palindrome number");
+        } else {
+            System.out.println(number + " is NOT a Palindrome number");
+        }
+    }
+}
