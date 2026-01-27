c698a9c9e6423775ab8f39d8229cf14eaa0c972b
Sakshi Hanwat <172370156+SakshiHanwat@users.noreply.github.com>
2026-01-27 16:38:31 +0530

    Create Day_0003_Fibonacci_509.java

---
+++ 100_Days_Leetcode/Day_0003_Fibonacci_509.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+
+class Solution {
+
+    // Fibonacci function (recursion)
+    public static int fib(int n) {
+
+        if (n == 0) return 0;
+        if (n == 1) return 1;
+
+        return fib(n - 1) + fib(n - 2);
+    }
+
+    // Main method
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter n: ");
+        int n = sc.nextInt();
+
+        int result = fib(n);
+        System.out.println("Fibonacci of " + n + " is: " + result);
+
+        sc.close();
+    }
+}
