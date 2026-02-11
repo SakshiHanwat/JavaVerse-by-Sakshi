b85df84f2a791fe4cf30c44df04e38ddeb433427
Sakshi Hanwat <172370156+SakshiHanwat@users.noreply.github.com>
2026-02-11 22:43:38 +0530

    Create Day_0018_IncreasingTripletSubsequences.java

---
+++ 100_Days_Leetcode/Day_0018_IncreasingTripletSubsequences.java
@@ -0,0 +1,32 @@
+public class Main {
+
+    public static boolean increasingTriplet(int[] nums) {
+
+        int first = Integer.MAX_VALUE;
+        int second = Integer.MAX_VALUE;
+
+        for (int num : nums) {
+
+            if (num <= first) {
+                first = num;
+            } 
+            else if (num <= second) {
+                second = num;
+            } 
+            else {
+                return true;
+            }
+        }
+
+        return false;
+    }
+
+    public static void main(String[] args) {
+
+        int[] nums = {5, 1, 5, 5, 2, 5, 4};
+
+        boolean result = increasingTriplet(nums);
+
+        System.out.println("Increasing Triplet Exists? " + result);
+    }
+}
