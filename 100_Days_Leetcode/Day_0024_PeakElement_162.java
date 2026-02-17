02d5313d90ebb038d8a76bb566a810a788a74eb3
Sakshi Hanwat <172370156+SakshiHanwat@users.noreply.github.com>
2026-02-17 10:04:31 +0530

    Create Day_0024_PeakElement_162.java

---
+++ 100_Days_Leetcode/Day_0024_PeakElement_162.java
@@ -0,0 +1,26 @@
+public class Day_0024_PeakElement_162 {
+
+    public static int findPeakElement(int[] nums) {
+        int n = 0;
+        int m = nums.length - 1;
+
+        while (n < m) {
+            int mid = n + (m - n) / 2;
+
+            if (nums[mid] > nums[mid + 1]) {
+                m = mid;
+            } else {
+                n = mid + 1;
+            }
+        }
+
+        return n;
+    }
+
+    public static void main(String[] args) {
+        int[] nums = {1, 2, 3, 1};
+
+        int result = findPeakElement(nums);
+        System.out.println("Peak element index: " + result);
+    }
+}
