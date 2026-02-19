af21f97c53d672b9cb820f20156428a9a3e0f579
Sakshi Hanwat <172370156+SakshiHanwat@users.noreply.github.com>
2026-02-19 09:06:33 +0530

    Create Day_0026_SingleElementInSortedArray.java

---
+++ 100_Days_Leetcode/Day_0026_SingleElementInSortedArray.java
@@ -0,0 +1,34 @@
+class Solution {
+
+    public int singleNonDuplicate(int[] nums) {
+
+        int start = 0;
+        int end = nums.length - 1;
+
+        while (start < end) {
+
+            int mid = start + (end - start) / 2;
+
+            // pair comparison using XOR
+            if (nums[mid] == nums[mid ^ 1]) {
+                start = mid + 1;
+            } else {
+                end = mid;
+            }
+        }
+
+        return nums[start];
+    }
+
+    // ----------- MAIN METHOD -----------
+    public static void main(String[] args) {
+
+        Solution obj = new Solution();
+
+        int[] nums1 = {1,1,2,3,3,4,4,8,8};
+        int[] nums2 = {3,3,7,7,10,11,11};
+
+        System.out.println(obj.singleNonDuplicate(nums1)); // Output: 2
+        System.out.println(obj.singleNonDuplicate(nums2)); // Output: 10
+    }
+}
