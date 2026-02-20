89e89817acc8926c73aab17254d61522b6e20452
Sakshi Hanwat <172370156+SakshiHanwat@users.noreply.github.com>
2026-02-20 15:51:35 +0530

    Create Day_0027_Sort_Colors_75.java

---
+++ 100_Days_Leetcode/Day_0027_Sort_Colors_75.java
@@ -0,0 +1,39 @@
+import java.util.Arrays;
+
+public class Day_0027_Sort_Colors_75 {
+
+    public void sortColors(int[] nums) {
+        int start = 0; 
+        int end = nums.length - 1;
+        int mid = 0;
+         
+        while (mid <= end) {
+            if (nums[mid] == 0) {
+                swap(nums, start, mid);
+                start++;
+                mid++;
+            } else if (nums[mid] == 1) {
+                mid++;
+            } else {
+                swap(nums, end, mid);
+                end--;
+            }
+        }
+    }
+
+    public void swap(int[] nums, int i, int j) {
+        int temp = nums[i];
+        nums[i] = nums[j];
+        nums[j] = temp;
+    }
+
+    public static void main(String[] args) {
+        Solution obj = new Solution();
+
+        int[] nums = {2, 0, 2, 1, 1, 0};
+
+        obj.sortColors(nums);
+
+        System.out.println("Sorted Array: " + Arrays.toString(nums));
+    }
+}
