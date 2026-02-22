65aa75a1265d789c4eca59b7d28ce8e500c3b160
Sakshi Hanwat <172370156+SakshiHanwat@users.noreply.github.com>
2026-02-22 07:54:42 +0530

    Create Day_0030_TrappingRainwater_42.java

---
+++ 100_Days_Leetcode/Day_0030_TrappingRainwater_42.java
@@ -0,0 +1,43 @@
+public class Day_0030_TrappingRainwater_42 {
+
+    static class Solution {
+        public int findMin(int[] nums) {
+            if (nums.length == 1) {
+                return nums[0];
+            } else if (nums[0] < nums[nums.length - 1]) {
+                return nums[0];
+            }
+
+            int start = 0;
+            int end = nums.length - 1;
+
+            while (start <= end) {
+                int mid = (start + end) / 2;
+
+                if (mid != 0 && nums[mid - 1] > nums[mid]) {
+                    return nums[mid];
+                } else if (mid != nums.length - 1 && nums[mid] > nums[mid + 1]) {
+                    return nums[mid + 1];
+                } else if (nums[start] <= nums[mid]) { // left part sorted
+                    start = mid + 1;
+                } else { // right part sorted
+                    end = mid - 1;
+                }
+            }
+            return -1;
+        }
+    }
+
+    public static void main(String[] args) {
+
+        Solution sol = new Solution();
+
+        int[] nums1 = {3, 4, 5, 1, 2};
+        int[] nums2 = {4, 5, 6, 7, 0, 1, 2};
+        int[] nums3 = {11, 13, 15, 17};
+
+        System.out.println("Minimum in nums1: " + sol.findMin(nums1));
+        System.out.println("Minimum in nums2: " + sol.findMin(nums2));
+        System.out.println("Minimum in nums3: " + sol.findMin(nums3));
+    }
+}
