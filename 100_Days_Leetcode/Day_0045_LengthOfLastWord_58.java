9f45d769d33c7ccdfafdc5bc2ab0137f1ca3469a
Sakshi Hanwat <172370156+SakshiHanwat@users.noreply.github.com>
2026-03-09 10:11:38 +0530

    Create Day_0045_LengthOfLastWord_58.java

---
+++ 100_Days_Leetcode/Day_0045_LengthOfLastWord_58.java
@@ -0,0 +1,20 @@
+class Day_0045_LengthOfLastWord_58 {
+    public int lengthOfLastWord(String s) {
+       int count = 0 ;
+
+       for(int i=s.length()-1;i>=0;i--){
+
+           char ch = s.charAt(i);
+
+           if(ch!=' '){
+               count++;
+           }else if ( count!=0){
+               break;
+           }
+
+       }
+
+       return count ;
+
+           }
+}
