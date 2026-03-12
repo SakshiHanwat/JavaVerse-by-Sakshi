8b5f316694bc91b5e63fab993cbfd66dc1b3c14f
Sakshi Hanwat <172370156+SakshiHanwat@users.noreply.github.com>
2026-03-12 10:17:45 +0530

    Create Day_0048_StringCompression_443.java

---
+++ 100_Days_Leetcode/Day_0048_StringCompression_443.java
@@ -0,0 +1,26 @@
+class Day_0048_StringCompression_443 {
+    public int compress(char[] chars) {
+        int i = 0;
+        int index = 0;
+
+        while (i < chars.length) {
+            char current = chars[i];
+            int count = 0;
+
+            while (i < chars.length && chars[i] == current) {
+                i++;
+                count++;
+            }
+
+            chars[index++] = current;
+
+            if (count > 1) {
+                for (char c : String.valueOf(count).toCharArray()) {
+                    chars[index++] = c;
+                }
+            }
+        }
+
+        return index;
+    }
+}
